59025411db88bf0a00a36005c18387d833000377
abdulre <abdulre8@gmail.com>
2026-04-02 20:01:11 +0100

    still debugging

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+console.log("ENV CHECK:", process.env.MONGO_URI); // 👈 ADD THIS
+
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ Mongo error:", err));
